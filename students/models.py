161de63e6c2be26427117e449f2bc49b50107851
Elham Wardak <elham.wardak.ew@gmail.com>
2023-03-16 11:58:55 +0430

    commit

--- students/models.py
+++ students/models.py
@@ -3,6 +3,17 @@ from django.db import models
 # Create your models here.
 
 
-class Students(models.Model):
+class Student(models.Model):
     name = models.CharField(max_length=150)
     age = models.IntegerField()
+    course = models.CharField(max_length=120)
+    is_passed = models.BooleanField(default=True)
+    last_name = models.CharField(max_length=80)
+    address = models.TextField(null=True, blank=True)
+    joined_on = models.DateTimeField()
+    
+
+
+    def __str__(self):
+        # return self.name +" - "+ str(self.id)
+        return f"{self.name}-{self.id}"
