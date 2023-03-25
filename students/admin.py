480d57ef15fce7b1040c7addc0b1772423218f1c
Elham Wardak <elham.wardak.ew@gmail.com>
2023-03-25 22:20:37 +0430

    25 march

--- students/admin.py
+++ students/admin.py
@@ -3,4 +3,11 @@ from students.models import Student
 
 # Register your models here.
 
-admin.site.register(Student)
+class StudentModalAdmin(admin.ModelAdmin):
+    list_display = ["id", "name","course"]
+    list_display_links = ["id","name"]
+   # list_per_page = 3
+    search_fields = ["name", "course"]
+    list_filter = ["joined_on","course"]
+
+admin.site.register(Student, StudentModalAdmin)
